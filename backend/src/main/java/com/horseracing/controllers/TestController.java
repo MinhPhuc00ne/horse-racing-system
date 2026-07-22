bb6adba7c017d60d005b19c95872026d09f9b56a
Minhvuu05 <phuongkaros@gmail.com>
2026-07-22 18:54:27 +0700

    feat: implement blacklist and ban test collections and expand betting flow scenarios

--- backend/src/main/java/com/horseracing/controllers/TestController.java
+++ backend/src/main/java/com/horseracing/controllers/TestController.java
@@ -57,4 +57,41 @@ public class TestController {
     public ResponseEntity<?> getAnyRole() {
         return ResponseEntity.ok(Map.of("message", "Any authenticated role content"));
     }
+
+    @GetMapping("/force-finish-race/{raceId}")
+    public ResponseEntity<?> forceFinishRace(
+            @org.springframework.web.bind.annotation.PathVariable Integer raceId,
+            @org.springframework.beans.factory.annotation.Autowired com.horseracing.repositories.RaceRepository raceRepository,
+            @org.springframework.beans.factory.annotation.Autowired com.horseracing.repositories.RaceSimulationRepository raceSimulationRepository,
+            @org.springframework.beans.factory.annotation.Autowired com.horseracing.repositories.RaceParticipantRepository raceParticipantRepository,
+            @org.springframework.beans.factory.annotation.Autowired com.horseracing.services.RefereeService refereeService) {
+        
+        try {
+            com.horseracing.entities.Race race = raceRepository.findById(raceId).orElseThrow();
+            race.setStatus("RUNNING");
+            raceRepository.save(race);
+
+            com.horseracing.entities.RaceSimulation sim = new com.horseracing.entities.RaceSimulation();
+            sim.setRace(race);
+            sim.setStatus("FINISHED");
+            sim.setStartTime(java.time.LocalDateTime.now().minusMinutes(5));
+            sim.setEndTime(java.time.LocalDateTime.now());
+            raceSimulationRepository.save(sim);
+
+            java.util.List<com.horseracing.entities.RaceParticipant> participants = raceParticipantRepository.findByRaceId(raceId);
+            for (int i = 0; i < participants.size(); i++) {
+                participants.get(i).setFinalRank(i + 1);
+                participants.get(i).setStatus("FINISHED");
+                raceParticipantRepository.save(participants.get(i));
+            }
+
+            refereeService.confirmResults(raceId);
+            
+            return ResponseEntity.ok(Map.of(
+                "message", "Race " + raceId + " forcefully finished and payouts distributed!"
+            ));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
 }
